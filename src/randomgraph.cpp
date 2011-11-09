4e7e3206562ee02ced24b7134be6b92e69961181
Kurt Horimoto <kkhorimoto@gmail.com>
2011-11-09 01:15:11 -0800

    Optimized random graph to O(M) time, but introduced the possibility of self-cycles and parallel edges.  If this becomes a problem, we can revert to v1.1 tag.

--- src/randomgraph.cpp
+++ src/randomgraph.cpp
@@ -1,66 +1,41 @@
 #include <fstream> 
 #include <iostream>
 #include <stdbool.h>
-#include <set>
+#include <string.h>
 #include "time.h"
 
 using namespace std;
 
-struct nodePair {
-    int start;
-    int end;
-};
+int numNodes, numEdges;
 
-//derived from c++ reference pages: http://www.cplusplus.com/reference/stl/set/set/
-struct compFn 
-{
-    bool operator () (const nodePair &a, const nodePair &b) {
-        if(a.start == b.start) {
-            return (a.end < b.end);
-        }
-        else return (a.start < b.start);   
-    }
-};
+inline int getRandomNodeNumber() {
+    short int firstHalf = rand();
+    short int secondHalf = rand();
+    unsigned int nodeNumber;
 
-void createRandomGraph(int numNodes, int numEdges, set<nodePair, compFn> &graphSet) {
-    while (numEdges > 0) {
-        nodePair n;
-        n.start = rand() % numNodes + 1;
-        n.end = rand() % numNodes + 1;
-        
-        if (n.start != n.end) {
-            int prevSize = graphSet.size();
-            graphSet.insert(n);
-            if (graphSet.size() == prevSize + 1)
-                numEdges--;
-        }
-    }
+    memcpy(&nodeNumber,&firstHalf,sizeof(firstHalf));
+    memcpy((short int *)(&nodeNumber) + 1, &secondHalf, sizeof(secondHalf));
+
+    return nodeNumber % numNodes + 1;
 }
-
 
 int main(int argc, char* argv[])
 {
     //initialize seed using time
     srand ( time(NULL) );
     
-    int numNodes, numEdges;
     sscanf(argv[1], "%d", &numNodes);
     sscanf(argv[2], "%d", &numEdges);
     char* outputFile = argv[3];
 
-    set<nodePair, compFn> graphSet;
-    createRandomGraph(numNodes, numEdges, graphSet);
-    
     ofstream os;
     os.open(outputFile);
     os << numNodes << "\n" << numEdges << "\n";
     
-    set<nodePair, compFn>:: iterator it;
-    for (it = graphSet.begin();it != graphSet.end(); it++) {
-        nodePair n = *it; 
-        os << n.start << " " << n.end << "\n";
+    for (int i = 0; i < numEdges; i ++) {
+        os << getRandomNodeNumber() << " " << getRandomNodeNumber() << "\n";
     }
-    
+
     os.close();
     return 0;
 }
