b80223edf552a7a06ccd5689adbef35da3fa3afe
Wesley Leung <wes.k.leung@gmail.com>
2011-11-08 21:33:34 -0800

    fixed sorting. works like a charm

--- src/randomgraph.cpp
+++ src/randomgraph.cpp
@@ -4,46 +4,37 @@
 #include <set>
 #include "time.h"
 
-
-
 using namespace std;
-
 
 struct nodePair {
     int start;
     int end;
 };
 
-struct compFn {
-    bool operator()(const nodePair a, const nodePair b) const 
-    {
-        if(a.start <= b.start && a.end <= b.end) {
-            return false;
+//derived from c++ reference pages: http://www.cplusplus.com/reference/stl/set/set/
+struct compFn 
+{
+    bool operator () (const nodePair &a, const nodePair &b) {
+        if(a.start == b.start) {
+            return (a.end < b.end);
         }
-        return true;
-    } 
+        else return (a.start < b.start);   
+    }
 };
-
 
 void createRandomGraph(int numNodes, int numEdges, set<nodePair, compFn> &graphSet) {
     while (numEdges > 0) {
-        
-      //  nodePair *n = (nodePair*)malloc(sizeof(nodePair));
-        
         nodePair n;
         n.start = rand() % numNodes + 1;
         n.end = rand() % numNodes + 1;
         
         if (n.start != n.end) {
             int prevSize = graphSet.size();
-            cout << "added: " << n.start << " " << n.end << "\n";
-            
             graphSet.insert(n);
             if (graphSet.size() == prevSize + 1)
                 numEdges--;
         }
     }
-
 }
 
 
@@ -52,26 +43,21 @@ int main(int argc, char* argv[])
     //initialize seed using time
     srand ( time(NULL) );
     
-    
     int numNodes, numEdges;
     sscanf(argv[1], "%d", &numNodes);
     sscanf(argv[2], "%d", &numEdges);
     char* outputFile = argv[3];
 
-    
     set<nodePair, compFn> graphSet;
     createRandomGraph(numNodes, numEdges, graphSet);
     
     ofstream os;
     os.open(outputFile);
-    
     os << numNodes << "\n" << numEdges << "\n";
     
     set<nodePair, compFn>:: iterator it;
-
     for (it = graphSet.begin();it != graphSet.end(); it++) {
-        nodePair n = *it;  //pointer?
-        cout << "printing: " << n.start << " " << n.end << "\n";
+        nodePair n = *it; 
         os << n.start << " " << n.end << "\n";
     }
     
