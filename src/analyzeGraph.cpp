d8794624fd0116f784bc5330a7c79ca96ca0e43c
Wesley Leung <wes.k.leung@gmail.com>
2011-11-11 03:38:08 -0800

    Fixed merge conflict.

--- src/analyzeGraph.cpp
+++ src/analyzeGraph.cpp
@@ -1,42 +1,19 @@
+#include <vector>
+#include <map>
 #include <fstream>
 #include <iostream>
 #include <cstdio>
 #include <stdbool.h>
 #include <string.h>
-#include <map>
+#include <set>
+
 
 using namespace std;
 int numNodes;
 
-void mapGraph (char *inputFile, map<int, int> &frequencyMap) {
-    ifstream file;
-    file.open(inputFile);
-
-    if(!file) {
-        cout << endl << "Error opening file " << inputFile; 
-        return;
-    }
-    
-    if(file.is_open()) {
-        int curr, trash;
-        file >> numNodes >> trash;
-        
-        while(!file.eof()) {
-            file >> curr >> trash;
-            int count = frequencyMap[curr];
-            if(count == 0) {
-                frequencyMap[curr] = 1;
-            } else {
-                int value = count + 1;
-                frequencyMap[curr] = value;
-            }
-        }
-    file.close();
-    }
-}
-
-int getHighestNodeInGraph (map<int, int> &graph) {
-    map<int, int>::iterator it;
+//Calculataes highest node
+int getHighestNodeInGraph (std::map<int, int>  &graph) {
+    map<int,int>::iterator it;
    
     int highestDegreeNode;
     int curr;
@@ -51,25 +28,76 @@ int getHighestNodeInGraph (map<int, int> &graph) {
     }
     return max;
 }
-        
+
+
+int mapGraph (char *inputFile, std::map<int, int>  &frequencyMap) {
+    ifstream file;
+    file.open(inputFile);
     
+    if(!file) {
+        cout << endl << "Error opening file " << inputFile; 
+        return 0;
+    }
+    
+    if(file.is_open()) {
+        int curr, trash;
+        file >> numNodes >> trash;
+        
+        while(!file.eof()) {
+            file >> curr >> trash;
+            int count = frequencyMap[curr];
+            
+            if(count == 0) {
+                frequencyMap[curr] = 1;
+            } else {
+                int updated = count+1;
+                frequencyMap[curr] = updated;
+                
+            }
+        }
+        
+        file.close();
+    }
+    return getHighestNodeInGraph(frequencyMap);
+}
+
+int findIsolatedNodes(char * inputFile) {
+    set<int>uniqueNodes;
+    
+    ifstream file;
+    file.open(inputFile);
+    if(file.is_open()) {
+        int curr, trash;
+        file >> numNodes >> trash;
+        while(!file.eof()) {
+            file >> curr;
+            uniqueNodes.insert(curr);
+        }
+    }
+    return numNodes - uniqueNodes.size();
+}
+
 int main(int argc, char* argv[]) {
     char* inputFile = argv[1];
     char* outputFile = argv[2];
-
     
     map<int, int> frequencyMap;
+    int highestDegreeNode =  mapGraph (inputFile, frequencyMap);
     
-    mapGraph (inputFile, frequencyMap);
-    highestDegreeNode = getHighestNodeInGraph(frequencyMap);
     
-
+    int numIsolatedNodes = findIsolatedNodes(inputFile);
+    
+    
     ofstream os;
     os.open("./analyzeGraphResults");
-    os << highestDegreeNode;
-        
+    
+    
+    os << highestDegreeNode << "\n";
+    os << numIsolatedNodes << "\n";
+    
         
     os.close();
     return 0;
 }
+
 
